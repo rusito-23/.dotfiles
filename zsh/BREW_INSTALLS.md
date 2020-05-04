4dee356ee04b0a7eaffe6fef804e1da54a347075
Igor Andruskiewitsch <i.andruskiewitsch23@gmail.com>
2020-05-04 12:15:38 -0300

    - zsh: moving sources into folder - adding systemextensions alias -

--- zsh/BREW_INSTALLS.md
+++ zsh/BREW_INSTALLS.md
@@ -1,11 +1,22 @@
 # Installs
 ## Stuff to install with brew:
 
+### Files
+
 - [fasd]()
 - [bat](https://github.com/sharkdp/bat)
+- [vifm]()
+
+### Media
+
 - [imagemagick]()
-- [pkg-config]()
-- [htop]()
+- [ffmpeg]()
+
+### Python
 - [pyenv]()
 - [pyenv-virtualenvwrapper]()
+
+### Misc
+- [pkg-config]()
+- [htop]()
 
--- zsh/src/main.zsh
+++ zsh/src/main.zsh
@@ -2,38 +2,16 @@
 source $HOME/.dotfiles/zsh/src/dirs.zsh
 
 # .ZSH CONFIGURATION
+
 # AUX FILES
 source $ASSETS_DIR/colors.zsh
-
-# : ALIAS
-source $SOURCES_DIR/alias.zsh
-
-# : INIT MESSAGE
-source $SOURCES_DIR/init.zsh
-
-# : Shell integration
 source ~/.iterm2_shell_integration.zsh
 
-# : VIRTUALENVWRAPPER (OPTIONAL)
-source $SOURCES_DIR/python.zsh
+# : INIT
+source $SOURCES_DIR/init.zsh
 
-# : FASD INITIALIZATION
-source $SOURCES_DIR/fasd.zsh
-
-# : GCLOUD INITIALIZATION
-source $SOURCES_DIR/gcloud.zsh
-
-# : GIT
-source $SOURCES_DIR/git.zsh
-
-# : GOLANG INITIALIZATION
-source $SOURCES_DIR/go.zsh
-
-# : AUTOSUGGESTIONS CONFIG
-source $SOURCES_DIR/autosuggestions.zsh
-
-# : TMUX UTILS
-source $SOURCES_DIR/tmux.zsh
+# : SOURCES
+for s in $SOURCES_DIR/sources/*; do source $s; done
 
 # : IGNORED
 for s in $SOURCES_DIR/ignored/*; do source $s; done
