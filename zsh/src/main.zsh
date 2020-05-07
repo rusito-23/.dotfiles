c21f6524f4bf3a621435169d2efcfd6cc85c51c4
Igor Andruskiewitsch <i.andruskiewitsch23@gmail.com>
2020-05-07 20:04:05 -0300

    - fix import order -

--- zsh/src/main.zsh
+++ zsh/src/main.zsh
@@ -5,13 +5,15 @@ source $HOME/.dotfiles/zsh/src/dirs.zsh
 
 # AUX FILES
 source $ASSETS_DIR/colors.zsh
-source ~/.iterm2_shell_integration.zsh
+if [ -f $SOURCES_DIR/custom.zsh ]; then
+    source ~/.iterm2_shell_integration.zsh
+fi
 
 # : INIT
 source $SOURCES_DIR/init.zsh
 
-# : SOURCES
-for s in $SOURCES_DIR/sources/*; do source $s; done
-
 # : IGNORED
 for s in $SOURCES_DIR/ignored/*; do source $s; done
+
+# : SOURCES
+for s in $SOURCES_DIR/sources/*; do source $s; done
